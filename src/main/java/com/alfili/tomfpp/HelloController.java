3081bc32c52620896528c1629678e947cf03fa43
alfili <146325432+alfili@users.noreply.github.com>
2023-12-10 19:43:59 +0700

    Open link on click (PP)

--- src/main/java/com/alfili/tomfpp/HelloController.java
+++ src/main/java/com/alfili/tomfpp/HelloController.java
@@ -2,17 +2,16 @@ package com.alfili.tomfpp;
 
 import com.alfili.tomfpp.models.FreeProxyCz;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
+import java.awt.*;
+import java.net.URI;
 import java.util.ArrayList;
+
 
 public class HelloController {
 
     private final ArrayList<String> parsingResultList = new ArrayList<>();
-
-    @FXML
-    private Label welcomeText;
 
     @FXML
     private TextArea resultTextArea;
@@ -26,6 +25,15 @@ public class HelloController {
 
         for(String link : this.parsingResultList) {
             resultTextArea.appendText(link);
+        }
+    }
+
+    @FXML
+    protected void onProxyShopProxy6LinkClick() {
+        try {
+            Desktop.getDesktop().browse(URI.create("https://proxy6.net/?r=585747"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.alfili.tomfpp {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.jsoup;
+    requires java.desktop;
 
     opens com.alfili.tomfpp to javafx.fxml;
     exports com.alfili.tomfpp;
